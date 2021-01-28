99487a2834d35d71ba8ae71cbfe0561e4504d0a9
javiermt98 <javiermtdam@gmail.com>
2021-01-28 21:30:10 +0100

    Examen V3

--- models/camp_model.py
+++ models/camp_model.py
@@ -13,10 +13,31 @@ class cooperativa(models.Model):
     socios = fields.Many2one("cooperativa.socios_model", "Socio")
     producto = fields.Many2one("cooperativa.prod_model", "Producto")
     cantidad = fields.Float(string="Cantidad", default=0)
+    active = fields.Boolean(readonly=True, default=True)
 
 
 
     @api.depends("cantidad")
-    def _cantidadPos(self):
+    def _cantidadPositiva(self):
         if self.cantidad<=0:
             raise ValidationError("La cantidad no puede ser negativa")
+
+
+    def actualizaKilos(self):
+        self.ensure_one()
+        for i in self.producto:
+            i.kgtot +=self.cantidad
+        
+               
+
+    def eliminaHistorial(self):
+        return True
+        # historialSocios = self.search([("active","=","False")])
+        # for i in historialSocios:
+        #     i.unlink()
+
+    def actualizaSaldo(self):
+        self.ensure_one()
+        for i in self.socios:
+            i.saldo += self.cantidad*self.producto.precio
+        self.active = False
--- models/prod_model.py
+++ models/prod_model.py
@@ -9,7 +9,7 @@ class cooperativa(models.Model):
     _name = 'cooperativa.prod_model'
     _description = 'Modelo de Productos'
     _sql_constraints = [('prod_name_uniq','UNIQUE (name)','No puede haber dos productos con el mismo nombre')]
-
+    
     name = fields.Char(string="Nombre", requred=True)
     descripcion = fields.Char(string="Descripcion", required=True)
     precio = fields.Float(string="Precio", required=True)
@@ -21,16 +21,8 @@ class cooperativa(models.Model):
         if self.precio<=0:
             raise ValidationError("El precio no puede ser negativo")
 
-    def _calcKg(self):
-        self.ensure_one()
-        suma = 0
-        for i in self.prod:
-            suma += i.cantidad
-        self.kgtot = suma
     
     def eliminaKg(self):
-        self.ensure_one()
-        for i in self.prod:
-            i.cantidad = 0
+        self.kgtot = 0
         
 
